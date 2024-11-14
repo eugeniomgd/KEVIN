e1837e20dfd2c41e7ebb283ddbbaf3251dc6de8b
eugeniomgd <84008699+eugeniomgd@users.noreply.github.com>
2024-11-14 14:00:39 +0100

    instalacion local

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\eugenio.garcia\OneDrive\AIProjects\OPERATIONS\venv
+set VIRTUAL_ENV=C:\Proyectos\KEVIN\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
